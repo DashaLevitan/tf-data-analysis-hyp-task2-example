da0998f3ecaba24e11d54824472a937aa35e45cb
DashaLevitan <90634932+DashaLevitan@users.noreply.github.com>
2023-04-12 21:06:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+#from scipy.stats import ks_2samp
+from scipy.stats import kstest
 
 
 chat_id = 808572568 # Ваш chat ID, не меняйте название переменной
@@ -10,5 +11,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha=0.06
-    stat, p_value = ks_2samp(x, y)
+    stat, p_value = kstest(x, y)
     return p_value < alpha # Ваш ответ, True или False
