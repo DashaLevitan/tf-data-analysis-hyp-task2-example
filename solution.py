5c41ed5cb416416ef459d6271aee12a21da2109c
DashaLevitan <90634932+DashaLevitan@users.noreply.github.com>
2023-04-12 11:43:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
@@ -8,4 +9,11 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    flag = 0
+    ks_stat, p_value = ks_2samp(x, y)
+
+    # сравнение p-value с уровнем значимости
+    alpha = 0.06
+    if p_value < alpha:
+        flag = 1
+    return flag # Ваш ответ, True или False
